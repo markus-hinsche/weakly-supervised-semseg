41a2acc90aa4bb1067503a451aa5013dbb3e1776
Markus Hinsche <m.hinsche@gmail.com>
2020-07-02 15:00:25 +0200

    Refactor putting global vars into a config.py file

---
+++ src/config.py
@@ -0,0 +1,8 @@
+IMAGE_DATA_DIR = "data/ISPRS_semantic_labeling_Vaihingen/top/"  # file e.g. top_mosaic_09cm_area1.tif
+GT_DIR = "data/ISPRS_semantic_labeling_Vaihingen_ground_truth_COMPLETE/"  # file e.g. top_mosaic_09cm_area1.tif
+
+IMAGE_DATA_TILES_DIR = "data/ISPRS_semantic_labeling_Vaihingen/top_tiles/"  # file e.g. top_mosaic_09cm_area1_tile1.tif
+GT_TILES_DIR = "data/ISPRS_semantic_labeling_Vaihingen_ground_truth_COMPLETE_tiles/"  # file e.g. top_mosaic_09cm_area1_tile1.tif
+
+HEIGHT_TILE = 200
+WIDTH_TILE = 200
--- src/split_to_tiles.py
+++ src/split_to_tiles.py
@@ -4,14 +4,7 @@ import os
 from PIL import Image
 from pathlib import Path
 
-IMAGE_DATA_DIR = "data/ISPRS_semantic_labeling_Vaihingen/top/"  # file e.g. top_mosaic_09cm_area1.tif
-GT_DIR = "data/ISPRS_semantic_labeling_Vaihingen_ground_truth_COMPLETE/"  # file e.g. top_mosaic_09cm_area1.tif
-
-IMAGE_DATA_TILES_DIR = "data/ISPRS_semantic_labeling_Vaihingen/top_tiles/"  # file e.g. top_mosaic_09cm_area1_tile1.tif
-GT_TILES_DIR = "data/ISPRS_semantic_labeling_Vaihingen_ground_truth_COMPLETE_tiles/"  # file e.g. top_mosaic_09cm_area1_tile1.tif
-
-HEIGHT_TILE = 200
-WIDTH_TILE = 200
+from .config import IMAGE_DATA_DIR, GT_DIR, IMAGE_DATA_TILES_DIR, GT_TILES_DIR, HEIGHT_TILE, WIDTH_TILE
 
 # Inspired by https://stackoverflow.com/a/7051075/5497962
 def crop(input: str, output_pattern: str, height: int, width: int):
@@ -29,7 +22,7 @@ def crop(input: str, output_pattern: str, height: int, width: int):
             tile.save(output_pattern % k)
             k += 1
 
-if __name__ == "__main__":
+def main():
     for fname in os.listdir(Path(IMAGE_DATA_DIR)):
         print(fname)
 
@@ -46,3 +39,6 @@ if __name__ == "__main__":
             width=WIDTH_TILE)
 
     # Result: 33 high-res are split into 4497 tiles
+
+if __name__ == "__main__":
+    main()

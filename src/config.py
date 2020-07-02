2b67464a92fdf8d7d4eb5413ff7597d49d34386e
Markus Hinsche <m.hinsche@gmail.com>
2020-07-02 15:52:22 +0200

    Prep labels for tile

--- src/config.py
+++ src/config.py
@@ -3,6 +3,7 @@ GT_DIR = "data/ISPRS_semantic_labeling_Vaihingen_ground_truth_COMPLETE/"  # file
 
 IMAGE_DATA_TILES_DIR = "data/ISPRS_semantic_labeling_Vaihingen/top_tiles/"  # file e.g. top_mosaic_09cm_area1_tile1.tif
 GT_TILES_DIR = "data/ISPRS_semantic_labeling_Vaihingen_ground_truth_COMPLETE_tiles/"  # file e.g. top_mosaic_09cm_area1_tile1.tif
+TILES_DIR = "data/ISPRS_semantic_labeling_Vaihingen/top_weaklabel_tiles/"  # file e.g. top_mosaic_09cm_area1_tile1_01100.tif
 
 HEIGHT_TILE = 200
 WIDTH_TILE = 200
---
+++ src/prep_tiles.py
@@ -0,0 +1,38 @@
+"""Create tile-level classification labels using the 5 classes
+1. Impervious surfaces - WHITE
+2. Building - BLUE
+3. Low vegetation - TURQUOISE
+4. Tree - GREEN
+5. Car - YELLOW
+6. Clutter/background - RED
+
+For instance a tile containing classes 1, 2, and 5, would have a label of [1, 1, 0, 0, 1].
+"""
+
+import os
+from PIL import Image
+from pathlib import Path
+import shutil
+
+from .config import IMAGE_DATA_DIR, GT_DIR, IMAGE_DATA_TILES_DIR, GT_TILES_DIR, TILES_DIR, LABELS, RED, BLACK
+
+
+def image_to_label(fpath_gt_tile):
+    im = Image.open(fpath_gt_tile)
+    distinct_pixel_values = set(im.getdata())
+
+    assert(distinct_pixel_values.issubset(LABELS + [RED, BLACK]))
+    label_vector = [int(color in distinct_pixel_values) for color in LABELS]
+    return label_vector
+
+
+if __name__ == "__main__":
+    for fname in os.listdir(GT_TILES_DIR):
+        fpath_gt_tile = Path(GT_TILES_DIR) / fname
+        label_vector = image_to_label(fpath_gt_tile)
+        label_string = "".join([str(l) for l in label_vector])
+        print(fname, label_string)
+
+        fpath_src = Path(IMAGE_DATA_TILES_DIR) / fname
+        fpath_dest = os.path.join(TILES_DIR, f"{fname[:-4]}_{label_string}.tif")  # file e.g. top_mosaic_09cm_area1_tile167_01100.tif
+        shutil.copy(fpath_src, fpath_dest)

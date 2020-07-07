a2b280386055751344ba3b69fbf638acbe3cb944
Markus Hinsche <m.hinsche@gmail.com>
2020-07-07 19:40:59 +0200

    Sum all the predictions for colors in color_vector (1), sum other predictions (0)

--- src/loss_custom.py
+++ src/loss_custom.py
@@ -3,7 +3,7 @@ from typing import Tuple, List
 import torch
 
 
-def get_target_indexes(label_vector: str) -> Tuple[List[int]]:
+def get_colors_for_image(label_vector: str) -> Tuple[List[int]]:
     """
     label_vector:
         '11001'
@@ -24,6 +24,7 @@ def get_target_indexes(label_vector: str) -> Tuple[List[int]]:
 
 
 class WeakCrossEntropy():
+    """Assumes that predictions have gone through a SoftmaxLayer"""
     def __init__(self, axis=1):
         self.axis = axis
         assert axis == 1
@@ -39,15 +40,21 @@ class WeakCrossEntropy():
         bs, ncolors, width, height = input.shape
         assert len(target) == bs, (len(target), bs)
 
+        # assert: prediction have gone through softmax
+        assert torch.isclose(input.sum(dim=1), torch.Tensor([1.])).all()
+
         # flatten the input
         input = input.reshape(bs, ncolors, -1)  # shape(bs, ncolors, width*height)
 
         # Get target indexes
-        colors_y_1, colors_y_0 = get_target_indexes(target[0])  # TODO do for bs
-        yHat_y_1 = input[0][colors_y_1].log() * -1.0
-        yHat_y_0 = (1 - input[0][colors_y_0]).log() * -1.0
+        colors_y_1, colors_y_0 = get_colors_for_image(target[0])  # TODO do for bs
+        sum_prob_y_1 = input[0][colors_y_1].sum(axis=0)
+        sum_prob_y_0 = input[0][colors_y_0].sum(axis=0)
 
-        item_losses = torch.cat([yHat_y_1, yHat_y_0], dim=0).sum(dim=0)
+        assert torch.isclose(sum_prob_y_1 + sum_prob_y_0, torch.Tensor([1.])).all()
+
+        item_losses = sum_prob_y_1.log() * -1.0
+        # (1 - sum_prob_y_0).log() * -1.0  # same as item_losses due to assert
 
         assert item_losses.shape[0] == width*height  # * bs ?
         return item_losses.mean()
--- src/test/test_loss_custom.py
+++ src/test/test_loss_custom.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from torch import nn
 from fastai.layers import CrossEntropyFlat
 
 from ..loss_custom import WeakCrossEntropy
@@ -10,19 +11,27 @@ def test_cross_entropy():
     bs = 2
     width = 200
     height = 200
+
     predictions = torch.rand(bs, n_classes, width, height, dtype=torch.float32)  # same shape as images
+    # apply Softmax along the ncolors dimension
+    predictions = nn.Softmax(dim=1)(predictions)
+
     ys = torch.empty([bs, 1, width, height]).random_(n_classes)
 
     loss = CrossEntropyFlat(axis=1)
     value = loss(predictions, ys.long())
     assert value>0
 
-def test_weak_cross_entropy():
+def test_weak_cross_entropy_basic():
     n_classes = 5
     bs = 2
     width = 200
     height = 200
+
     predictions = torch.rand(bs, n_classes, width, height, dtype=torch.float32)  # same shape as images
+    # apply Softmax along the ncolors dimension
+    predictions = nn.Softmax(dim=1)(predictions)
+
     ys = ['11001', '01001']
 
     loss = WeakCrossEntropy(axis=1)
@@ -34,7 +43,11 @@ def test_weak_cross_entropy_all_classes():
     bs = 2
     width = 200
     height = 200
+
     predictions = torch.rand(bs, n_classes, width, height, dtype=torch.float32)  # same shape as images
+    # apply Softmax along the ncolors dimension
+    predictions = nn.Softmax(dim=1)(predictions)
+
     ys = ['11111', '11111']
 
     loss = WeakCrossEntropy(axis=1)
@@ -48,11 +61,11 @@ def test_weak_cross_entropy_one_color_correct():
     width = 10
     height = 10
 
-    # always predict color WHITE
+    # always predict a specific color
     predictions = torch.zeros(bs, n_classes, width, height, dtype=torch.float32)  # same shape as images
-    predictions[:, 0, :, :] = 1
+    predictions[:, 3, :, :] = 1
 
-    ys = ['10000', '10000']
+    ys = ['00010', '10010']
 
     loss = WeakCrossEntropy(axis=1)
     value = loss(predictions, ys)
@@ -65,7 +78,7 @@ def test_weak_cross_entropy_one_color_wrong():
     width = 10
     height = 10
 
-    # always predict color WHITE
+    # always predict a specific color
     predictions = torch.zeros(bs, n_classes, width, height, dtype=torch.float32)  # same shape as images
     predictions[:, 1, :, :] = 1
 

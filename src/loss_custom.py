Fix order of softmax, sum, log; avoid nan by clipping probabilities

src/loss_custom.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 
 
-HIGH_LOSS = 7.
+THRESH_LOWER_CLIP_PROBS = 0.001
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
@@ -29,21 +29,30 @@ class WeakCrossEntropy():
         bs, ncolors, width, height = input_orig.shape
         assert target.shape == (bs, ncolors)
 
-        input = input_orig.log_softmax(dim=1)
+        input = input_orig.softmax(dim=1)
 
         # flatten the input
         input = input.reshape(bs, ncolors, -1)  # shape(bs, ncolors, width*height)
 
-        target_mask = target.repeat(width*height, 1, 1).transpose(0, 1).transpose(1,2)
+        if torch.isnan(input).any():
+            print("input is nan: ", input)
+
+        target_mask = target.repeat(width*height, 1, 1).transpose(0, 1).transpose(1,2)  # shape(bs, ncolors, width*height)
 
         sums_prob_y_1 = (input*target_mask).sum(axis=1)  # shape (bs, width*height, )
-        item_losses = sums_prob_y_1 * -1.0  # shape (bs, width*height, )
+
+        # Clipping low probabilities
+        sums_prob_y_1[sums_prob_y_1 < THRESH_LOWER_CLIP_PROBS] = THRESH_LOWER_CLIP_PROBS
+
+        item_losses = sums_prob_y_1.log() * -1.0  # shape (bs, width*height, )
 
         assert item_losses.shape == (bs, width*height)
 
-        # Clip loss
-        item_losses[torch.isinf(item_losses)] = HIGH_LOSS
+        assert not torch.isinf(item_losses).any()
 
         loss = item_losses.mean()
+
+        if torch.isnan(loss).any():
+            print("loss is nan, ", sums_prob_y_1)
 
         return loss
15a41ea3bb697580daa3d9262f56bcf4d1024700
Markus Hinsche <m.hinsche@gmail.com>
2020-07-11 09:41:19 +0200

    Move acc_satellite metric from notebook to module

--- src/metrics_custom.py
+++ src/metrics_custom.py
@@ -1,12 +1,23 @@
 import torch
 
+from src.config import LABELS, RED, BLACK
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 
+name2id = {v:k for k,v in enumerate(LABELS+[RED, BLACK])}  # {WHITE:0, BLUE:1}
+void_codes_red = name2id[RED]
+void_codes_black = name2id[BLACK]
+
+
+def acc_satellite(input, target):
+    target = target.squeeze(1)
+    mask = target != void_codes_red
+    mask = target != void_codes_black
+    return (input.argmax(dim=1)[mask]==target[mask]).float().mean()
+
 
 def acc_weakly(input, target):
-
     bs, ncolors, width, height = input.shape
     input = input.reshape(bs, ncolors, -1)
     input = input.argmax(dim=1)  # shape: (bs, pixel)
--- src/test/test_metrics_custom.py
+++ src/test/test_metrics_custom.py
@@ -1,8 +1,28 @@
 import torch
 
-from ..metrics_custom import acc_weakly
+from ..metrics_custom import acc_satellite, acc_weakly
 from ..config import CODES
 
+
+def test_acc_satellite():
+    n_classes = 5
+    bs = 2
+    width = 3
+    height = 3
+
+    predictions = torch.zeros(bs, n_classes, width, height, dtype=torch.float32)
+    # Set color=3 for all pixels
+    predictions[:, 3, :, :] = 0.5
+    # Set color=1 for pixels of a specific width
+    predictions[:, 1, 1, :] = 0.6
+
+    # Set color=3 for all pixels
+    ys = torch.ones(bs, 1, width, height, dtype=torch.float32)*3
+    # Set color=1 for pixels of a specific width
+    ys[:, 0, 1, :] = 1
+
+    accuracy = acc_satellite(predictions, ys)
+    assert torch.isclose(accuracy, torch.Tensor([1.]))
 
 def test_acc_weakly():
     n_classes = 5
@@ -16,8 +36,6 @@ def test_acc_weakly():
 
     # Set color=1 for pixels of a specific width
     predictions[:, 1, 1, :] = 0.6
-
-    # print(predictions.argmax(dim=1).reshape(bs, -1))
 
     ys = torch.tensor([[0,1,0,1,0], [0,1,0,0,0]])
     accuracy = acc_weakly(predictions, ys)

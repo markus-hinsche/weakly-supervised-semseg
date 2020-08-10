b1e30b2f299b2412b08dff599c2dcf96345e0567
Markus Hinsche <m.hinsche@gmail.com>
2020-08-10 10:57:02 +0200

    Add docstrings to metrics

--- src/metrics_custom.py
+++ src/metrics_custom.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch
 
 from src.config import LABELS, RED, BLACK
@@ -10,22 +12,40 @@ void_codes_red = name2id[RED]
 void_codes_black = name2id[BLACK]
 
 
-def acc_satellite(input, target):
+def acc_satellite(input: torch.tensor, target: torch.tensor) -> torch.tensor:
+    """Calculate accuracy for semantic segmentation
+
+    Args:
+        input: Predictions
+        target: Mask with label per pixel
+
+    Returns:
+        One value for the mean accuracy
+    """
     target = target.squeeze(1)
     mask = target != void_codes_red
     mask = target != void_codes_black
     return (input.argmax(dim=1)[mask]==target[mask]).float().mean()
 
 
-def acc_weakly(input, target):
-    bs, ncolors, width, height = input.shape
-    input = input.reshape(bs, ncolors, -1)
-    input = input.argmax(dim=1)  # shape: (bs, pixel)
+def acc_weakly(input: torch.tensor, target: List[int]) -> torch.tensor:
+    """Calculate a metric based on the tile-level labels and per-pixel predictions
 
-    mat = torch.zeros(input.shape).to(device)
+    Args:
+        input: Predictions
+        target: 0 or 1 for each of the 5 colors
+
+    Returns:
+        One value for the mean accuracy
+    """
+    bs, ncolors, width, height = input.shape
+    input_ = input.reshape(bs, ncolors, -1)
+    input_ = input_.argmax(dim=1)  # shape: (bs, pixel)
+
+    mat = torch.zeros(input_.shape).to(device)
     for batch_idx in range(bs):
         for i in range(ncolors):
             if target[batch_idx][i] == 0:
                 continue
-            mat[batch_idx][input[batch_idx]==i] = 1
+            mat[batch_idx][input_[batch_idx]==i] = 1
     return mat.float().mean()

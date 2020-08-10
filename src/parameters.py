4dd05adb3630735534d535b6362fae187f1777f3
Markus Hinsche <m.hinsche@gmail.com>
2020-08-10 18:19:43 +0200

    Make resnet type a parameter called BACKBONE

--- src/parameters.py
+++ src/parameters.py
@@ -1,3 +1,4 @@
+import torchvision.models as models
 
 # 0.5 means half width, and half height, so will have 1/4 of pixels
 IMG_SIZE_RATIO = 0.5
@@ -7,3 +8,5 @@ WEIGHT_DECAY = 0.1
 
 LEARNING_RATE_FS = 3e-4
 LEARNING_RATE_WS = 1e-3
+
+BACKBONE = models.resnet18  # resnet34  # resnet101

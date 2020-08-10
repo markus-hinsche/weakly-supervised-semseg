41d11553ab48617db38be00b5926bb2d1ec5ae0a
Markus Hinsche <m.hinsche@gmail.com>
2020-08-10 11:29:46 +0200

    Fix: add missing test_data file

--- src/test/test_utils.py
+++ src/test/test_utils.py
@@ -7,7 +7,7 @@ from ..util import get_y_colors, REGEX_IMG_FILE_NAME, REGEX_IMG_FILE_NAME, REGEX
 from ..config import WHITE, BLUE, YELLOW
 
 def test_get_y_colors():
-    path = Path(__file__).parent / "test_data" /  "tile_dir" / "top_mosaic_09cm_area1_tile1_11001.tif"
+    path = Path(__file__).parent / "test_data" / "top_mosaic_09cm_area1_tile1_11001.tif"
     expected = [WHITE, BLUE, YELLOW]
     actual = get_y_colors(path)
     assert set(actual) == set(expected)

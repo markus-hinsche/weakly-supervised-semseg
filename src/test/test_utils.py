2201e5ac2cc5fa9aa96cebe145c796c745cd2fd4
Markus Hinsche <m.hinsche@gmail.com>
2020-08-12 12:14:04 +0200

    Add tests

--- src/test/test_utils.py
+++ src/test/test_utils.py
@@ -3,7 +3,7 @@ import math
 import os, os.path
 from pathlib import Path
 
-from ..util import get_y_colors, REGEX_IMG_FILE_NAME, REGEX_IMG_FILE_NAME, REGEX_IMG_FILE_NAME_WITH_LABEL_VECTOR
+from ..util import get_y_colors, REGEX_IMG_FILE_NAME, REGEX_IMG_FILE_NAME, REGEX_IMG_FILE_NAME_WITH_LABEL_VECTOR, is_in_set_n1, has_a_valid_color
 from ..constants import WHITE, BLUE, YELLOW
 
 def test_get_y_colors():
@@ -11,6 +11,10 @@ def test_get_y_colors():
     expected = [WHITE, BLUE, YELLOW]
     actual = get_y_colors(path)
     assert set(actual) == set(expected)
+
+def test_has_a_valid_color():
+    path = Path(__file__).parent / "test_data" / "top_mosaic_09cm_area38_tile1_11001.tif"
+    assert has_a_valid_color(path)
 
 def test_regex_file_name():
     fname = "top_mosaic_09cm_area27_tile154.tif"
@@ -24,3 +28,11 @@ def test_regex_file_name_with_label_vector():
     assert match_result.group('tile_id') == "154"
     assert match_result.group('area_id') == "27"
     assert match_result.group('label_vector') == "11001"
+
+def test_is_in_set_n1_true():
+    path = Path(__file__).parent / "test_data" / "top_mosaic_09cm_area1_tile1.tif"
+    assert is_in_set_n1(path, regex_obj=REGEX_IMG_FILE_NAME)
+
+def test_is_in_set_n1_false():
+    path = Path(__file__).parent / "test_data" / "top_mosaic_09cm_area38_tile1.tif"
+    assert not is_in_set_n1(path, regex_obj=REGEX_IMG_FILE_NAME)
